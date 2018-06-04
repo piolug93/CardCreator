917b3f7707b0eed88945ca29acb003b7b711bd80
Piotr Ługowski <piolug93@gmail.com>
2018-06-04 11:41:09 +0200

    Remove: impletation Initilizable empty Constructor Segmented: Implementation openFileChooser and connection database

--- src/main/java/Controller/MainController.java
+++ src/main/java/Controller/MainController.java
@@ -10,10 +10,8 @@ import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 
 import java.io.File;
-import java.net.URL;
-import java.util.ResourceBundle;
 
-public class MainController implements Initializable {
+public class MainController {
     @FXML private VBox editContent;
     @FXML private TextField keyTemplate;
     @FXML private TextField valueTemplate;
@@ -23,15 +21,6 @@ public class MainController implements Initializable {
     @FXML private ProgressIndicator progress;
     private File database;
 
-
-    public MainController() {
-
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-    }
-
     @FXML
     public void buttonAddAction(ActionEvent e) {
         editContent.getChildren().add(new FieldValueEditor(keyTemplate.getText(), valueTemplate.getText(), editContent));
@@ -39,11 +28,7 @@ public class MainController implements Initializable {
 
     @FXML
     private void openDatabase(ActionEvent e) {
-        database = new FileChooser().showOpenDialog(menuOpenDatabase.getParentPopup().getScene().getWindow());
-        if(database != null) {
-            DriverConnection.connect(database.getPath());
-            buttonUpdate.setDisable(false);
-        }
+        openDialogSelectDatabase();
     }
 
     @FXML
@@ -68,5 +53,17 @@ public class MainController implements Initializable {
         doneAlert.setHeaderText("");
         doneAlert.setContentText("Update has completed");
         doneAlert.showAndWait();
+    }
+
+    private void openDialogSelectDatabase() {
+        database = new FileChooser().showOpenDialog(menuOpenDatabase.getParentPopup().getScene().getWindow());
+        if(database != null) {
+            connectionToDatabase();
+        }
+    }
+
+    private void connectionToDatabase() {
+        DriverConnection.connect(database.getPath());
+        buttonUpdate.setDisable(false);
     }
 }
